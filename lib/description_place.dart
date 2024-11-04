7da7d974b8542eabd6122f29d7704f0f48ac448e
Grois333 <isaacgroisman@gmail.com>
2024-11-04 19:08:31 +0200

    added column

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class DescriptionPlace extends StatelessWidget {
+
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
+
+
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    final star_half = Container (
+      margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+
+      child: Icon(
+        Icons.star_half,
+        color:  Color(0xFFf2C611),
+      ),
+    );
+
+    // ignore: unused_local_variable
+    final star_border = Container (
+      margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+
+      child: Icon(
+        Icons.star_border,
+        color:  Color(0xFFf2C611),
+      ),
+    );
 
     final star = Container (
       margin: EdgeInsets.only(
@@ -27,7 +62,7 @@ class DescriptionPlace extends StatelessWidget {
           ),
 
           child: Text(
-            "Duwili Ella",
+            namePlace,
             style: TextStyle(
               fontSize: 30.0,
               fontWeight: FontWeight.w900
@@ -43,7 +78,7 @@ class DescriptionPlace extends StatelessWidget {
             star,
             star,
             star,
-            star
+            star_half
           ],
         )
 
@@ -51,9 +86,30 @@ class DescriptionPlace extends StatelessWidget {
       ],
     );
 
-    
+    final description = Container(
+      margin: new EdgeInsets.only(
+          top: 20.0,
+          left: 20.0,
+          right: 20.0
 
-    return title_stars;
+      ),
+      child: new Text(
+        descriptionPlace,
+        style: const TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF56575a)
+        ),
+
+      ),
+    );
+
+    return Column(
+      children: <Widget>[
+        title_stars,
+        description
+      ],
+    );
 
 
   }
