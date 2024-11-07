552f5149319f10007d8996b29d345c7bcca49f7c
Grois333 <isaacgroisman@gmail.com>
2024-11-07 14:42:47 +0200

    added bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'review_list.dart';
 //import 'gradient_back.dart';
 import 'package:flutter/services.dart';
 import 'header_appbar.dart';
+import 'face_trips.dart';
 
 void main(){
   SystemChrome.setSystemUIOverlayStyle(
@@ -17,7 +18,7 @@ void main(){
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  String descriptionDummy = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,21 +35,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        //body: new DescriptionPlace("Bahamas", 4, descriptionDummy),
-        body: Stack(
-          children: <Widget>[
-            ListView(
-              children: <Widget>[
-                DescriptionPlace("Bahamas", 4, descriptionDummy),
-                ReviewList()
-
-              ],
-            ),
-            HeaderAppBar()
-          ],
-        ),
-      )//MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FaceTrips()
     );
   }
 }
