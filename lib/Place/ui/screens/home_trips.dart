11fba3cd2d2faa5d78205ebe0a3548ad05ad8edb
Grois333 <isaacgroisman@gmail.com>
2024-11-10 18:17:07 +0200

    arranging files

--- lib/Place/ui/screens/home_trips.dart
+++ lib/Place/ui/screens/home_trips.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:facetrip/Place/ui/widgets/description_place.dart';
-import 'package:facetrip/header_appbar.dart';
+import '/Place/ui/screens/header_appbar.dart';
 import 'package:facetrip/Place/ui/widgets/review_list.dart';
 
 class HomeTrips extends StatelessWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import '/Place/ui/widgets/description_place.dart';
 import '/Place/ui/widgets/review_list.dart';
 //import 'gradient_back.dart';
 import 'package:flutter/services.dart';
-import 'header_appbar.dart';
+import '/Place/ui/screens/header_appbar.dart';
 import 'face_trips.dart';
 import 'face_trips_cupertino.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:facetrip/User/bloc/bloc_user.dart';
 
 void main(){
   SystemChrome.setSystemUIOverlayStyle(
@@ -22,22 +24,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: FaceTripsCupertino()
-    );
+    return BlocProvider(
+        child: MaterialApp(
+          title: 'FaceTrip',
+          home: FaceTripsCupertino(),
+        ),
+        bloc: UserBloc());
   }
 }
 
